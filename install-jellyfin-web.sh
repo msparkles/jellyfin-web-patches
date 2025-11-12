2c84d1bec13077e6a92d61ee52f60131d8a16a6f
Madeline Sparkles <madeline@mouse.lgbt>
2025-11-12 16:25:17 +0100

    add custom styles

--- install-jellyfin-web.sh
+++ install-jellyfin-web.sh
@@ -28,11 +28,27 @@ echo "Project dir: $PROJDIR"
 echo "Installing to: $INSTALL"
 
 cd "$PROJDIR/jellyfin-web" || exit
+git fetch || exit
+git reset --hard || exit
+git restore . || exit
 git checkout "release-$MAJOR.$MINOR.z" || exit
 git reset origin/"release-$MAJOR.$MINOR.z" --hard || exit
 git apply --verbose "$PROJDIR/patches"/* || exit
 
+source /usr/share/nvm/init-nvm.sh
+nvm install 20
+nvm use 20
+
 npm install || exit
+
+mkdir -p ./src/styles/custom
+wget  -O ./src/styles/custom/catppuccin.css 			  https://jellyfin.catppuccin.com/theme.css
+wget  -O ./src/styles/custom/catppuccin-macchiato.css  https://jellyfin.catppuccin.com/catppuccin-macchiato.css
+
+wget  -O ./src/styles/custom/noseyrodent.css	https://the.sqky.one/.assets/fonts/noseyrodent.css
+mkdir -p ./src/styles/custom/noseyrodent
+wget  -O ./src/styles/custom/noseyrodent/noseyrodent-Regular.woff2 https://the.sqky.one/.assets/fonts/noseyrodent/noseyrodent-Regular.woff2
+wget  -O ./src/styles/custom/noseyrodent/noseyrodent-Bold.woff2 https://the.sqky.one/.assets/fonts/noseyrodent/noseyrodent-Bold.woff2
 
 sudo npm run build:production || exit
 sudo mkdir -p "$INSTALL" || exit
